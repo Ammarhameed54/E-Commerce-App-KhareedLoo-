1201bb44e3bef40e577993d29c2890e67cd047f6
Ammar Hameed <f21bdats1m02054@iub.edu.pk>
2024-06-22 03:32:01 -0700

    Commit

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -13,8 +13,34 @@ class _ProductDetailState extends State<ProductDetail> {
     return Scaffold(
       backgroundColor: const Color(0xFFFEF5F1),
       body: Container(
-        child: const Column(
-          children: [],
+        padding: const EdgeInsets.only(top: 60),
+        child:  Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children:[ GestureDetector(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: const Icon(Icons.arrow_back_rounded, size: 40,),
+                )),
+                Center(child: Image.asset("images/product_images/headphones.png", height: 400,))
+           ] ),
+
+           Expanded(
+             child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: const Text("Hello This is My first Product Which Is Headphones ",),
+             ),
+           )
+            
+          ],
         ),
       ),
     );
